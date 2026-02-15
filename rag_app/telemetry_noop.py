20e203543850a201a56585942a7685d53ef91b5f
lizhian01 <xueyongsheng75@gmail.com>
2026-02-15 21:45:31 +0800

    避免 Chroma遥测上报和相关报错

---
+++ rag_app/telemetry_noop.py
@@ -0,0 +1,8 @@
+﻿from overrides import override
+from chromadb.telemetry.product import ProductTelemetryClient, ProductTelemetryEvent
+
+
+class NoopTelemetry(ProductTelemetryClient):
+    @override
+    def capture(self, event: ProductTelemetryEvent) -> None:
+        return
